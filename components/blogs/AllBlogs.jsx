88bcee804b67f42acd8a8db799d274986960a65f
AYENI DANIEL <starlordflash@gmail.com>
2023-04-27 16:51:52 +0100

    updated the mobile view

--- components/blogs/AllBlogs.jsx
+++ components/blogs/AllBlogs.jsx
@@ -76,8 +76,8 @@ const data = [
 const AllBlogs = () => {
   return (
     <ContainerLayout>
-      <div className="w-full flex flex-col mt-20">
-        <h1 className="mt-16 text-[#555] text-[40px] mb-8 ">Older posts</h1>
+      <div className="w-full flex flex-col mt-20 mb-8 md:mb-0">
+        <h1 className="mt-16 text-[#555] text-2xl md:text-[40px] mb-8 ">Older posts</h1>
         <div className="w-full grid grid-cols-1 gap-x-8 gap-y-12  sm:grid-cols-2 lg:grid-cols-3 flow-hide">
           {data.map((item, index) => (
             <div
@@ -89,13 +89,13 @@ const AllBlogs = () => {
               <Image
                 src={item.image}
                 alt="image"
-                className="w-8/12 h-auto rounded-[30px]"
+                className="w-11/12 mx-auto md:w-8/12 h-auto rounded-[30px]"
               />
-              <p className="text-base text-[#555] mt-4 ">{item.date}</p>
-              <p className="text-[32px] text-[#555] leading-[40px] mt-4">
+              <p className="text-[12px] md:text-base text-[#555] mt-4 ">{item.date}</p>
+              <p className="text-2xl md:text-[32px] text-[#555] leading-[40px] mt-4">
                 {item.title}
               </p>
-              <p className="text-xl text-[#555] mt-4">{item.content}</p>
+              <p className="text-sm md:text-xl text-[#555] mt-4">{item.content}</p>
               <p className="bg-mainBlue  text-base text-white  font-semibold mt-4 rounded-[30px] w-fit px-6 py-3 ">
                 Blog
               </p>
--- components/blogs/BlogHero.jsx
+++ components/blogs/BlogHero.jsx
@@ -13,19 +13,19 @@ const BlogHero = () => {
 
   return (
     <ContainerLayout>
-      <div className="w-full flex flex-col">
+      <div className="w-full flex flex-col pt-2">
         <div>
           <Image
             src={logo}
             alt="logo"
-            className="w-[172px] h-auto cursor-pointer"
+            className="w-[120px] md:w-[172px] h-auto cursor-pointer"
             onClick={() => router.push("/")}
           />
         </div>
-        <div className="w-full flex items-center mt-12 ">
-          <h1 className="w-4/12 text-[64px] text-[#333]">Our Blog</h1>
-          <div className="flex items-center justify-between flex-row-reverse md:flex-row w-8/12">
-            <div className="flex items-center border-b pb-2  border-[#999] w-6/12 ">
+        <div className="w-full flex md:flex-row flex-col items-center mt-12  ">
+          <h1 className="w-full md:w-4/12 text-3xl md:text-[64px] text-[#333]">Our Blog</h1>
+          <div className="flex items-center justify-between flex-row-reverse md:flex-row w-full md:w-8/12 mt-4 md:mt-0">
+            <div className="flex items-center border-b pb-2  border-[#999] w-full md:w-6/12 ">
               <input
                 type="text"
                 className="border-none outline-none focus:outline-none focus:border-none text-base placeholder:text-[#555] flex-grow  "
@@ -34,17 +34,17 @@ const BlogHero = () => {
               <BiSearch className="text-lg text-[#444]" />
             </div>
             {/* searc */}
-            <div className="w-4/12 flex items-center space-x-8 ">
+            <div className="md:w-4/12 hidden md:flex items-center space-x-8 ">
               <Link
                 href="/"
-                className="border border-[#999] text-lg text-[#999] rounded-[20px] px-6 w-fit h-[55px] flex items-center space-x-2 cursor-pointer"
+                className="border border-[#999] text-sm md:text-lg text-[#999] rounded-[20px] px-4 md:px-6  h-auto md:h-[55px] flex items-center space-x-2 cursor-pointer"
                 onClick={() => router.push("/")}
               >
-                <RiArrowLeftSLine className="text-2xl" />
+                <RiArrowLeftSLine className="text-xl md:text-2xl" />
                 <p>Go back</p>
               </Link>
 
-              <p className="text-mainBlue border-l-2 pl-3 text-base border-[#555] font-semibold ">
+              <p className="text-mainBlue border-l-2 pl-3 text-base border-[#555] font-semibold hidden md:block ">
                 Blog
               </p>
             </div>
@@ -52,21 +52,22 @@ const BlogHero = () => {
             {/* button */}
           </div>
         </div>
-        <h1 className="mt-16 text-[#555] text-[40px] ">Latest</h1>
-        <div className="w-full mt-8 flex justify-between items-center">
-          <div className="w-6/12 items-center">
+        {/* <div className='w-full h-[1px] mt-10 bg-[#999]' /> */}
+        <h1 className="mt-16 text-[#555] text-xl md:text-[40px] ">Latest</h1>
+        <div className="w-full mt-8 flex flex-col md:flex-row justify-between items-center">
+          <div className="w-full md:w-6/12 items-center">
             <Image
               src={heroImage}
               alt="hero"
               className="w-full h-auto rounded-[30px]"
             />
           </div>
-          <div className="w-5/12 flex flex-col">
-            <p className="text-base text-[#555] ">April 12 . 10:52 PM</p>
-            <h2 className="md:w-11/12 w-full text-[40px] font-[500] text-[#333] my-7">
+          <div className="w-full md:w-5/12 flex flex-col">
+            <p className="text-[12px] md:text-base text-[#555] mt-6 ">April 12 . 10:52 PM</p>
+            <h2 className="md:w-11/12 w-full text-[40px] font-[500] text-[#333] my-7 text-2xl">
               Collecting data smarter with WeCollect.
             </h2>
-            <p className="text-[#555] text-xl">
+            <p className="text-[#555] text-sm md:text-xl">
               Lorem ipsum dolor sit amet consectetur. Arcu massa sit ornare
               diam. Phasellus nisi auctor viverra pellentesque. In ut
               condimentum nisl eu. Ut viverra et mattis sem nibh ipsum...
